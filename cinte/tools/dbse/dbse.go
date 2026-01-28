5c8187ec16514d71ac3718140865136deb1faff3
Stephen Malone <mail@wirehaiku.org>
2026-01-28 14:39:53 +1000

    Added dbse.Close function.

--- cinte/tools/dbse/dbse.go
+++ cinte/tools/dbse/dbse.go
@@ -8,6 +8,15 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// Close closes an active database connection.
+func Close(db *sqlx.DB) error {
+	if err := db.Close(); err != nil {
+		return fmt.Errorf("cannot close database - %w", err)
+	}
+
+	return nil
+}
+
 // Connect returns a new SQLite database connection with an executed query.
 func Connect(path, code string) (*sqlx.DB, error) {
 	db, err := sqlx.Connect("sqlite3", path)
@@ -16,6 +25,7 @@ func Connect(path, code string) (*sqlx.DB, error) {
 	}
 
 	if _, err := db.Exec(code); err != nil {
+		Close(db)
 		return nil, fmt.Errorf("cannot initialise database %q - %w", path, err)
 	}
 
--- cinte/tools/dbse/dbse_test.go
+++ cinte/tools/dbse/dbse_test.go
@@ -3,8 +3,19 @@ package dbse
 import (
 	"testing"
 
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
+
+func TestClose(t *testing.T) {
+	// setup
+	db := sqlx.MustConnect("sqlite3", ":memory:")
+
+	// success
+	err := Close(db)
+	assert.NoError(t, err)
+}
 
 func TestConnect(t *testing.T) {
 	// setup
